3afaa909e6af011af3a434a178c369766f0c1275
LIZELUAN <104906671+LIZELUAN@users.noreply.github.com>
2024-01-31 14:52:04 +0800

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,11 @@
+# any password I want
+password=123456
+# pull image
+docker pull rocker/rstudio:4.3.1
+
+# Run container, and Rstudio can be assessed by http://localhost:8888
+docker run --rm -p 8888:8787 \
+-e PASSWORD=$password \
+rocker/rstudio:4.3.1
+
+# open the broswer and login with username "rstudio" and password "123456", and that's it!
